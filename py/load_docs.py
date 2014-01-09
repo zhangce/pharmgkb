949e76f1d3a41a350144bf342f0097e190dc1c66
zhangce <czhang@cs.wisc.edu>
2014-01-08 21:42:02 -0800

    a

--- py/load_docs.py
+++ py/load_docs.py
@@ -6,24 +6,24 @@ from dstruct.Document import *
 from dstruct.Word import *
 
 for row in get_inputs():
-    docid = row["docids.docid"]
-    folder = row["docids.folder"]
-    log(docid)
-    doc = Document(docid)
+  docid = row["docids.docid"]
+  folder = row["docids.folder"]
+  log(docid)
+  doc = Document(docid)
 
-    try:
-      for l in open(folder + "/input.text"):
-        ss = l.rstrip().split('\t')
-        if len(ss) < 3: continue
-        (insent_id, word, pos, ner, lemma, deppath, deppar, sentid, box) = ss
-        doc.push_word(Word(insent_id, word, pos, ner, lemma, deppath, deppar, sentid, box))
-    except:
-      continue
+  try:
+    for l in open(folder + "/input.text"):
+      ss = l.rstrip().split('\t')
+      if len(ss) < 3: continue
+      (insent_id, word, pos, ner, lemma, deppath, deppar, sentid, box) = ss
+      doc.push_word(Word(insent_id, word, pos, ner, lemma, deppath, deppar, sentid, box))
+  except:
+    continue
 
-    print json.dumps({
-      "docid":docid, 
-      "document":serialize(doc)
-      })
+  print json.dumps({
+    "docid":docid, 
+    "document":serialize(doc)
+  })
 
 
 
